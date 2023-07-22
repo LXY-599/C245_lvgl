e9d06d30fe579e24e99dbabc0dbf4f726302b29b
LXY <xinyao.luo@foxmail.com>
2023-07-22 18:42:05 +0800

    init c245_lvgl

---
+++ lvgl/porting/lv_port_indev.h
@@ -0,0 +1,53 @@
+
+/**
+ * @file lv_port_indev_templ.h
+ *
+ */
+
+/*Copy this file as "lv_port_indev.h" and set this value to "1" to enable content*/
+#if 1
+
+#ifndef LV_PORT_INDEV_H
+#define LV_PORT_INDEV_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lvgl/lvgl.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+typedef struct
+{
+    uint16_t count;
+    short int diff;
+} ec11_data_t;
+extern ec11_data_t ec11_data;
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_port_indev_init(void);
+
+void encoder_func(bool value);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_PORT_INDEV_TEMPL_H*/
+
+#endif /*Disable/Enable content*/
